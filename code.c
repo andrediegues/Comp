d52b36b0e9447ed4cf0e09ea876cfbe47b452500
andrediegues <andrediegues@github.com>
2016-11-29 15:58:31 +0000

    built constructors

--- code.c
+++ code.c
@@ -1,15 +1,26 @@
 #include "code.h"
 
-mkInstr(op, a1, a2, a3){
-	...
+Instr mkInstr(op, a1, a2, a3){
+  Instr *instr = (Instr*) malloc(sizeof(Instr));
+  instr -> op = op;
+  instr -> addr1 = a1;
+  instr -> addr2 = a2;
+  instr -> addr3 = a3;
+  return instr;
 }
 
-mkIInt(int i){
-	...
+Address mkIInt(int i){
+  Address *address = (Address*) malloc(sizeof(Address));
+  address -> kind = I_Int;
+  address -> content.val = i;
+  return address;
 }
 
-mkIVar(char* x){
-	...
+Address mkIVar(char* x){
+  Address *address = (Address*) malloc(sizeof(Address));
+  address -> kind = I_VAR;
+  address -> content.var = x;
+  return address;
 }
 
 mkIEmpty(){
@@ -17,34 +28,34 @@ mkIEmpty(){
 }
 
 List mkList(Inst head, List tail){
-	List* newp;
-	newp = (List*) malloc(sizeof(List));
-	newp -> inst = head;
-	newp -> next = tail;
-	return newp; 
+  List *newp;
+  newp = (List*) malloc(sizeof(List));
+  newp -> inst = head;
+  newp -> next = tail;
+  return newp; 
 }
 
 List append(List* l1, List* l2){
-	List* p;
-	if(l1 == NULL)
-		return l2;
-	if(l2 == NULL)
-		return l1;
-	for(p = l1; tail(p) != NULL; p = tail(p))
-		tail(p) = l2;
-	return l1;
+  List* p;
+  if(l1 == NULL)
+    return l2;
+  if(l2 == NULL)
+    return l1;
+  for(p = l1; tail(p) != NULL; p = tail(p))
+    tail(p) = l2;
+  return l1;
 }
 
 
 
-Inst head(List l){
-	if(l != NULL)
-		return (l -> inst);
+Instr head(List l){
+  if(l != NULL)
+    return (l -> inst);
 }
 
 List tail(List l){
-	if(l != NULL)
-		return (l -> next;)
+  if(l != NULL)
+    return (l -> next);
 }
 
 Pair compileExpr(Expr e);
@@ -52,13 +63,24 @@ InstrList compileCmd(Cmd c);
 InstrList compileCmdList(CmdList l);
 
 InstrList compileCmd(Cmd c){
-	...
-	switch(cmd... -> kind){
-		case ASSIGN:
-			p = compileExpr(cmd... -> expr);
-			l = append(p -> code/*snd(p)*/, mkList(mkInstr(ASSIGN, cmd -> var/*var(cmd)*/, p -> var/*fst(p)*/, NULL)));
-			return l;
-		case IF:
-		...
-	}
+  ...
+  switch(cmd... -> kind){
+  case ASSIGN:
+    p = compileExpr(cmd... -> expr);
+    l = append(p -> code/*snd(p)*/, mkList(mkInstr(ASSIGN, cmd -> var/*var(cmd)*/, p -> var/*fst(p)*/, NULL)));
+    return l;
+  case IF:
+    ...
+    }
 }
+
+InstrList compileCmdList(CmdList l){
+  InstrList il = (InstrList*) malloc(sizeof(InstrList));
+  
+}
+
+Pair compileExpr(Expr expr){
+  Pair p = (Pair*) malloc(sizeof(Pair));
+  
+}
+
--- code.h
+++ code.h
@@ -1,26 +1,32 @@
 typedef enum{
-	I_PLUS, I_MINUS, I_DIV, I_TIMES, I_ASSIGN, I_IF, I_GOTO //devem ser diferentes dos ja definidos
+  I_PLUS, I_MINUS, I_DIV, I_TIMES, I_ASSIGN, I_IF, I_GOTO //devem ser diferentes dos ja definidos
 }OpKind;
 
 typedef enum{ 
-	I_INT, I_VAR, I_EMPTY
+  I_INT, I_VAR, I_EMPTY
 }AddrKind;
 
 typedef struct{
-	AddrKind kind;
-	union{
-		int val;
-		char* var;
-	}content;
+  AddrKind kind;
+  union{
+    int val;
+    char* var;
+  }content;
 }Address;
 
 typedef struct{
-	OpKind op;
-	Address addr1, addr2, addr3;
+  OpKind op;
+  Address addr1, addr2, addr3;
 }Instr;
 
-typedef struct List list;
-struct List{
-	Instr inst;
-	List next;
+typedef struct{ 
+  Address first;
+  InstrList* second; //Lista ligada de instrucoes de codigo intermedio
+}Pair;
+
+typedef struct _List *List;
+struct _List{
+  Instr inst;
+  List next;
 }
+  
